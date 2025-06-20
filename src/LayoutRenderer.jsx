64d9ab9ad7c2516732cab91917d5732ef835cb43
Srikanth Pagadala <srikanth.pagadala@outlook.com>
2025-06-20 16:29:56 +0530

    Update LayoutRenderer.jsx

--- src/LayoutRenderer.jsx
+++ src/LayoutRenderer.jsx
@@ -1,16 +1,36 @@
-{layout.map(({ id, component, type, row, col, colSpan = 1 }) => {
-  const Comp = allComponents[component];
-  const gridStyles = {
-    gridRow: row + 1,
-    gridColumn: `${col + 1} / span ${colSpan}`,
-    border: '1px solid #ccc',
-    padding: '8px',
-    minHeight: '100px',
+import { allComponents } from './allComponents';
+import { ClientOnly } from './ClientOnly';
+
+export const LayoutRenderer = ({ layout, rows, cols }) => {
+  const gridStyle = {
+    display: 'grid',
+    gridTemplateRows: `repeat(${rows}, 1fr)`,
+    gridTemplateColumns: `repeat(${cols}, 1fr)`,
+    gap: '10px',
   };
 
   return (
-    <div key={id} style={gridStyles}>
-      {type === 'client' ? <ClientOnly componentName={component} /> : <Comp />}
+    <div style={gridStyle}>
+      {layout.map(({ id, component, type, row, col, colSpan = 1 }) => {
+        const Comp = allComponents[component];
+        const gridStyles = {
+          gridRow: row + 1,
+          gridColumn: `${col + 1} / span ${colSpan}`,
+          border: '1px solid #ccc',
+          padding: '8px',
+          minHeight: '100px',
+        };
+
+        return (
+          <div key={id} style={gridStyles}>
+            {type === 'client' ? (
+              <ClientOnly componentName={component} />
+            ) : (
+              <Comp />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
-})}
+};
